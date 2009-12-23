711e8ba8f47eb74e9c84b9bf1f99a693f88ee36b
Matthew Wild <mwild1@gmail.com>
2009-12-23 01:00:13 +0000

    plugins.ping: Update to handle errors passed from Verse

--- plugins/ping.lua
+++ plugins/ping.lua
@@ -4,8 +4,12 @@ function riddim.plugins.ping(bot)
 	bot:hook("commands/ping", function (command)
 			local jid = command.param;
 			if jid then
-				bot.stream:ping(jid, function (time)
-					command:reply(string.format("Pong from %s in %0.3f seconds", jid, time));
+				bot.stream:ping(jid, function (time, jid, error)
+					if time then
+						command:reply(string.format("Pong from %s in %0.3f seconds", jid, time));
+					else
+						command:reply("Ping failed ("..(error.condition or "unknown reason")..")"..(error.text and (": "..error.text) or ""));
+					end
 				end);
 				return true;
 			end
