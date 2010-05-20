065ae6d967a664aa6770583f9fdfd6a6f5076626
Matthew Wild <mwild1@gmail.com>
2010-05-20 14:37:28 +0100

    Rename plugins.presence to plugins.autosubscribe

--- plugins/autosubscribe.lua
+++ plugins/autosubscribe.lua
@@ -1,5 +1,5 @@
 
-function riddim.plugins.presence(bot)
+function riddim.plugins.autosubscribe(bot)
 	bot:hook("presence", function (presence)
 		if presence.stanza.attr.type ~= 'subscribe' then return nil; end
 		bot:send_presence(presence.sender.jid, 'subscribed');
