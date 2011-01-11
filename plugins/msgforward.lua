df3645ed417c4115a679ec5ff72bc5519b885956
Kim Alvefur <zash@zash.se>
2011-01-11 10:40:12 +0100

    plugins.msgforward: Clone messages, not just the body.

--- plugins/msgforward.lua
+++ plugins/msgforward.lua
@@ -1,6 +1,8 @@
 function riddim.plugins.msgforward(bot)
 	local compare_jid = require "util.jid".compare;
+	local st_clone = require "util.stanza".clone;
 	local forwards = bot.config.forwards or {};
+
 	bot:hook("message", function(event)
 		local message = event.stanza;
 		local from = message.attr.from;
@@ -9,7 +11,9 @@ function riddim.plugins.msgforward(bot)
 		if not body then return end
 		for jid, room in pairs(forwards) do
 			if compare_jid(from, jid) and bot.rooms[room] then
-				bot.rooms[room]:send_message(body);
+				local out = st_clone(message);
+				out.attr.to, out.attr.from, out.attr.type = nil, nil, "groupchat";
+				bot.rooms[room]:send(out);
 				return true;
 			end
 		end
