545a452f32de78f817cbb79626b26cf22ccb79d3
Kim Alvefur <zash@zash.se>
2011-03-09 18:04:36 +0100

    plugins.xkcd: Exact matches takes priority over others

--- plugins/xkcd.lua
+++ plugins/xkcd.lua
@@ -37,12 +37,15 @@ function handle_xkcd_command(command)
 		if not xkcdnum then return "Please supply an XKCD number or a search string :)"; end
 			if not tonumber(xkcdnum) then -- Search for an xkcd
 				xkcdnum = xkcdnum:gsub("[()]", function(s) return "%" .. s end)
+					:lower()
 					:gsub("[%[]",function(s) return "%" .. s end)
 					:gsub("%%(%b[])",function(s) return (#s > 2 and "" or "%") .. s end);
 				local results = {};
 				for x, xkcd in pairs(xkcd_list) do
 					name = xkcd:lower()
-					if name:match(xkcdnum:lower()) then
+					if name == xkcdnum then -- exact match
+						return xkcd..", http://xkcd.org/"..x.."/";
+					elseif name:match(xkcdnum) then
 						table.insert(results, x);
 						--return commands.xkcd(msg, x);
 					end
