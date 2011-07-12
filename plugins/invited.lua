ef72e0322fd8147716c1a44fa422eaf408ee4d41
Kim Alvefur <zash@zash.se>
2011-07-12 15:39:25 +0200

    plugins.invited: Move xmlns-es to variables.

--- plugins/invited.lua
+++ plugins/invited.lua
@@ -1,13 +1,17 @@
+local xmlns_muc = "http://jabber.org/protocol/muc";
+local xmlns_muc_user = xmlns_muc .. "#user";
+local xmlns_jxc = "jabber:x:conference";
+
 function riddim.plugins.invited(bot)
 	bot:hook("message", function(event)
-		local x = event.stanza:get_child("x", "http://jabber.org/protocol/muc#user");
+		local x = event.stanza:get_child("x", xmlns_muc_user);
 		if x then -- XEP 45
 			local invite = x:get_child("invite");
 			if invite then
 				bot:join_room(event.stanza.attr.from);
 			end
 		else -- try XEP 249
-			x = event.stanza:get_child("x", "jabber:x:conference");
+			x = event.stanza:get_child("x", xmlns_jxc);
 			if x and x.attr.jid then
 				bot:join_room(x.attr.jid);
 			end
