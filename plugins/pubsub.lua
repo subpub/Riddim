bf8be616461daa8174848ff84c56f4568f5aa089
Kim Alvefur <zash@zash.se>
2011-08-21 02:50:20 +0200

    plugins.pubsub: Remove

--- plugins/pubsub.lua
+++
@@ -1,9 +0,0 @@
-function riddim.plugins.pubsub(bot)
-	bot.stream:add_plugin("pubsub");
-	bot.pubsub = bot.stream.pubsub;
-
-	-- Maybe pubsub/event/ns/element or something?
-	bot.stream:hook("pubsub/event", function(event)
-		return bot:event("pubsub/event", event);
-	end);
-end
