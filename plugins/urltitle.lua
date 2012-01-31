7f92f37a6df949a6800bb200fd03f193893901b4
Kim Alvefur <zash@zash.se>
2012-01-31 19:29:02 +0100

    Import urltitle plugin (thanks ruskie)

---
+++ plugins/urltitle.lua
@@ -0,0 +1,27 @@
+function riddim.plugins.urltitle(bot)
+	require "net.httpclient_listener";
+	local http = require "net.http";
+
+	local function handler(message)
+		local url = message.body and message.body:match("https?://%S+");
+		if url then
+			http.request(url, nil, function (data, code)
+				if code ~= 200 then return end
+				local title = data:match("<title[^>]*>([^<]+)");
+
+				if title then
+					title = title:gsub("\n", "");
+					if message.room then
+						message.room:send(title);
+					else
+						message:reply(title);
+					end
+				end
+			end);
+		end
+	end
+	bot:hook("message", handler);
+	bot:hook("groupchat/joined", function(room)
+		room:hook("message", handler)
+	end);
+end
